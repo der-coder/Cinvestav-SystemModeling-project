9b93f279ad3921568002aa63a613588ee99f02dd
Enrique Benavides Tellez <enrique.b.tellez@gmail.com>
2019-10-30 19:58:10 -0600

    pendulin con valores del midstrom

--- codigosM/pendulin.m
+++ codigosM/pendulin.m
@@ -6,12 +6,9 @@ close all
 
 %% Variables para ODE45
 
-
-tspan = 0:1/30:5;
-
-l = 2.5;        % Largo del pendulo (m)
-m = 5;          % Masa de la bola (kg)
-k = 0;          % Coeficiente de friccion
+l = 0.3;        % Largo del pendulo (m)
+m = 0.12166;    % Masa de la bola (kg)
+k = 0.1;        % Coeficiente de friccion
 theta = pi/2;
 
 p0 = [theta 0];         %theta, d_theta
@@ -19,32 +16,30 @@ p0 = [theta 0];         %theta, d_theta
 [t,thet] = ode45(@(t,y)pendulo(t,y,l,m,k),tspan,p0);
 
 figure('Name','Poscicion y Velocidad')
-plot(t,thet(:,1),'k',t,thet(:,2),'k--','linewidth',1)
+plot(t,thet(:,1),'k',t,thet(:,2),'k--','linewidth',3)
 xlabel('Tiempo','Interpreter','latex','fontsize',16)
-ylabel('$/theta / \dot{?theta}$','Interpreter','latex','fontsize',16)
-legend({'$x$','$\dot{x}$'},'Interpreter','latex','Location','Southeast','fontsize',14)
+ylabel('$\theta / \dot{\theta}$','Interpreter','latex','fontsize',16)
+legend({'$\theta$','$\dot{\theta}$'},'Interpreter','latex','Location','Southeast','fontsize',14)
 title('Posicion/Velocidad péndulo','fontsize',18)
 set(gcf,'Color',[1 1 1])
 grid on
 
 figure('Name','Retrato Fase')
 
-plot(thet(:,1),thet(:,2),'k','linewidth',1)
+plot(thet(:,1),thet(:,2),'k','linewidth',3)
+xlabel('Posicion ($\theta$)','Interpreter','latex','fontsize',14)
+ylabel('Velocidad ($\dot{\theta}$)','Interpreter','latex','fontsize',14)
+title('Retrato fase $\theta / \dot{\theta}$','Interpreter','latex','fontsize',16)
 
-xlabel('Posicion (x)','Interpreter','latex','fontsize',14)
-ylabel('Velocidad ($\dot{x}$)','Interpreter','latex','fontsize',14)
-title('Retrato fase $x / \dot{x}$','Interpreter','latex','fontsize',16)
 set(gcf,'Color',[1 1 1])
 grid on
-
 
 %% Simulación de pendulo
 X = l*sin(thet(:,1));
 Y = -l*cos(thet(:,1));
-simulacion(t,X,Y,l)
+% simulacion(t,X,Y,l)
 
 end
-
 
 %% Funcion para ODE45
 function dx = pendulo(~,x,l,m,k)
@@ -63,9 +58,9 @@ x_max = max(x);
 y_min = min(y);
 y_max = max(y);
 
-str0 = string(strcat('t = ',num2str(round(t,2))));
-str1 = string(strcat('x = ',num2str(round(x,2))));
-str2 = string(strcat('y = ',num2str(round(y,2))));
+str0 = string(strcat('t = ',num2str(round(t,4))));
+str1 = string(strcat('x = ',num2str(round(x,4))));
+str2 = string(strcat('y = ',num2str(round(y,4))));
 
 fig = figure(3);
 set(gcf, 'Position', get(0, 'Screensize'));
@@ -78,15 +73,15 @@ n = size(t,1);
 for i = 1:n
     cla(gca(fig));
     plot([0 x(i)],[0 y(i)],'k');
-    text(l-.25,-l,str0(i),'Fontsize',16)
-    text(l-.25,-l-.10,str1(i),'Fontsize',16)
-    text(l-.25,-l-.20,str2(i),'Fontsize',16)
+    text(l+.25,-l,str0(i),'Fontsize',16)
+    text(l+.25,-l-.10,str1(i),'Fontsize',16)
+    text(l+.25,-l-.20,str2(i),'Fontsize',16)
     pos = [x(i) y(i)];
-    viscircles(pos,.1);
+    viscircles(pos,.05);
     drawnow
 %     k(i) = getframe(fig);
 
-    pause(1/30)
+%pause(1/30)
 
 end
 % video = VideoWriter('Pendulo', 'MPEG-4');
